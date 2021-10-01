ec0119251271664b18abbabf675f46b86b82d581
Dyllan To <me@dyllan.to>
2021-10-01 02:28:51 -0700

    Cache loaded prices

--- hydra/SimManager.py
+++ hydra/SimManager.py
@@ -2,12 +2,20 @@ import pathlib
 import pandas as pd
 import pyarrow.parquet as pq
 
+price_cache = {}
 
-def load_prices(pair, path, startDate=None, endDate=None, interval=1) -> pd.DataFrame:
-    filepath = pathlib.Path(__file__).parent.absolute()
-    parqpath = filepath.joinpath(path, f"{pair}_{interval}.filled.parq")
-    table = pq.read_table(parqpath)
-    prices = table.to_pandas()
+
+def load_prices(
+    pair, path="../data/kraken", startDate=None, endDate=None, interval=1
+) -> pd.DataFrame:
+    filename = f"{pair}_{interval}"
+    prices = price_cache.get(filename, None)
+    if prices is None:
+        filepath = pathlib.Path(__file__).parent.absolute()
+        parqpath = filepath.joinpath(path, f"{filename}.filled.parq")
+        table = pq.read_table(parqpath)
+        prices = table.to_pandas()
+        price_cache[filename] = prices
     # print(prices.index)
     if not isinstance(prices.index, pd.DatetimeIndex):
         prices["time"] = pd.to_datetime(prices["time"], unit="s")

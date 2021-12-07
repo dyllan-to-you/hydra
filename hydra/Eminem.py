4d3719aa51aedb465af6fd3d9f7e9b7d26617496
Dyllan To <me@dyllan.to>
2021-12-06 22:40:09 -0800

    Improve file saving, factor `process_root_layer`

--- hydra/Eminem.py
+++ hydra/Eminem.py
@@ -15,7 +15,8 @@ from hydra.Environments import gen_tasks
 from hydra.Environments import main as bulk_analysis
 from hydra.Environments import run_parallel
 
-ROOT_WINDOW = 2 * 1440
+ROOT_WINDOW = 90 * 1440
+OVERLAP = 0.95
 SKIP_SAVE = False
 ABORT_ON_DUPLICATE = False
 
@@ -26,7 +27,7 @@ def main(
     startDate=None,
     runEndDatetime=None,
     pair_binance="BTCUSD",
-    overlap=0.75,
+    overlap=OVERLAP,
     forceNewFile=True,
     config="config",
     rootWindow=ROOT_WINDOW,
@@ -66,23 +67,9 @@ def main(
 
             startDate = last_run + pd.to_timedelta("1d") - rootWindow_td
             print(f"Last Run: {last_run} {time_since_last_run} ago")
-            result_data, result_charts, result_aggregates = bulk_analysis(
-                startDate,
-                end=runEndDatetime,
-                window=f"{rootWindow}min",
-                detrend=True,
-                pair=pair_binance,
-                midnightLock=True,
-            )
-            catchup_parent_data: pd.DataFrame = save_results(
-                result_data, result_charts, rootWindow=rootWindow, output_dir=output_dir
-            )
-            catchup_parent_data = preprocess_child_runs(
-                overlap,
-                rootWindow,
-                runEndDatetime,
-                catchup_parent_data,
-                all_children=True,
+
+            catchup_parent_data: pd.DataFrame = process_root_layer(
+                startDate, runEndDatetime, pair_binance, overlap, rootWindow, output_dir
             )
             accumulated_parent_data = accumulated_parent_data.append(
                 catchup_parent_data
@@ -92,23 +79,9 @@ def main(
         print(
             f"Generating First Run {startDate} - {runEndDatetime}",
         )
-        result_data, result_charts, result_aggregates = bulk_analysis(
-            startDate,
-            end=runEndDatetime,
-            window=f"{rootWindow}min",
-            detrend=True,
-            pair=pair_binance,
-            midnightLock=True,
-        )
-        accumulated_parent_data: pd.DataFrame = save_results(
-            result_data, result_charts, output_dir=output_dir
-        )
-        accumulated_parent_data = preprocess_child_runs(
-            overlap,
-            rootWindow,
-            runEndDatetime,
-            accumulated_parent_data,
-            all_children=True,
+
+        accumulated_parent_data = process_root_layer(
+            startDate, runEndDatetime, pair_binance, overlap, rootWindow, output_dir
         )
 
     # history = set(accumulated_parent_data.itertuples(index=False, name=None))
@@ -244,93 +217,29 @@ def main(
     accumulated_parent_data["saved"] = True
     save_accumulated_parents(output_dir, accumulated_parent_data)
 
-    """
-                                                   parent_startDate    parent_endDate  parent_window  child_minPerCycle
-figname                                                                                             
-(D)BTCUSD 2013-10-07 00:01 - 2013-10-08 00:0014... 2013-10-07 00:01:00 2013-10-08    1440      14.39
-(D)BTCUSD 2013-10-08 00:01 - 2013-10-09 00:0014... 2013-10-08 00:01:00 2013-10-09    1440      14.39
-(D)BTCUSD 2013-10-09 00:01 - 2013-10-10 00:0014... 2013-10-09 00:01:00 2013-10-10    1440      14.39
-(D)BTCUSD 2013-10-10 00:01 - 2013-10-11 00:0014... 2013-10-10 00:01:00 2013-10-11    1440    1440.00
-(D)BTCUSD 2013-10-10 00:01 - 2013-10-11 00:0014... 2013-10-10 00:01:00 2013-10-11    1440      14.39
-...                                                                ...        ...     ...        ...
-(D)BTCUSD 2021-11-22 00:01 - 2021-11-23 00:0014... 2021-11-22 00:01:00 2021-11-23    1440      14.39
-(D)BTCUSD 2021-11-23 00:01 - 2021-11-24 00:0014... 2021-11-23 00:01:00 2021-11-24    1440    1440.00
-(D)BTCUSD 2021-11-23 00:01 - 2021-11-24 00:0014... 2021-11-23 00:01:00 2021-11-24    1440      14.39
-(D)BTCUSD 2021-11-24 00:01 - 2021-11-25 00:0014... 2021-11-24 00:01:00 2021-11-25    1440    1440.00
-(D)BTCUSD 2021-11-24 00:01 - 2021-11-25 00:0014... 2021-11-24 00:01:00 2021-11-25    1440      14.39
-    """
     return
 
 
-def save_accumulated_parents(
-    output_dir, accumulated_parent_data, filename="accumulated_parents"
+def process_root_layer(
+    startDate, runEndDatetime, pair_binance, overlap, rootWindow, output_dir
 ):
-    path = output_dir / f"{filename}.parq"
-    print(f"Saving Accumulated Parent Data to {path}")
-    # print(accumulated_parent_data)
-    if not SKIP_SAVE:
-        try:
-            accumulated_parent_data_tosave = accumulated_parent_data.copy()
-            accumulated_parent_data_tosave[
-                "child_window"
-            ] = accumulated_parent_data_tosave["child_window"] / pd.to_timedelta("60s")
-
-            accumulated_parent_data_tosave.to_parquet(path)
-
-            if RUN_SAVE_TEST:
-                loaded = load_accumulated_parents(output_dir, filename)
-                print("a", accumulated_parent_data)
-                print(loaded)
-                # print(f"SAVE TEST! \n{accumulated_parent_data.compare(loaded)}")
-                assert_frame_equal(accumulated_parent_data, loaded)
-                # assert accumulated_parent_data.equals(
-                #     loaded
-                # ), f"SAVE TEST FAILED! \n{accumulated_parent_data=} \n{loaded=}"
-
-        except pyarrow.ArrowNotImplementedError as e:
-            print(accumulated_parent_data.dtypes)
-            raise e
-
-
-def load_accumulated_parents(output_dir, filename="accumulated_parents"):
-    print(f"Loading previous runs from {filename}")
-    accumulated_parents = pd.read_parquet(output_dir / f"{filename}.parq")
-    accumulated_parents["child_window"] = pd.to_timedelta(
-        accumulated_parents["child_window"], unit="min"
+    result_data, result_charts, result_aggregates = bulk_analysis(
+        startDate,
+        end=runEndDatetime,
+        window=f"{rootWindow}min",
+        detrend=True,
+        pair=pair_binance,
+        midnightLock=True,
     )
-
-    # print(accumulated_parents)
-    return accumulated_parents
-
-
-def partition_filename_factory(window, prefix="", suffix=""):
-    def cb(keys):
-        # year, month, day = keys
-        return f"{prefix}{window}{suffix}.parq"
-
-    return cb
-
-
-def save_results(result_data, result_charts, rootWindow=ROOT_WINDOW, output_dir=None):
-    # if isinstance(window, str):
-    #     window_minutes = pd.to_timedelta(window).total_seconds() / 60
-    # else:
-    #     window_minutes = window
-
-    result_data_df = save_result_data(output_dir, result_data, rootWindow=rootWindow)
-    extra_data_df = save_extra_data(output_dir, result_charts, rootWindow=rootWindow)
-
-    accumulated_parent_data: pd.DataFrame = result_data_df[
-        ["startDate", "endDate", "window_original", "window", "minPerCycle"]
-    ]
-    accumulated_parent_data = accumulated_parent_data.rename(
-        columns={
-            "startDate": "parent_startDate",
-            "endDate": "parent_endDate",
-            "window_original": "parent_window_original",
-            "window": "parent_window",
-            "minPerCycle": "child_minPerCycle",
-        }
+    accumulated_parent_data: pd.DataFrame = save_results(
+        result_data, result_charts, rootWindow=rootWindow, output_dir=output_dir
+    )
+    accumulated_parent_data = preprocess_child_runs(
+        overlap,
+        rootWindow,
+        runEndDatetime,
+        accumulated_parent_data,
+        all_children=True,
     )
 
     return accumulated_parent_data
@@ -451,6 +360,80 @@ def preprocess_child_runs(
     return accumulated_parent_data
 
 
+def save_accumulated_parents(
+    output_dir, accumulated_parent_data, filename="accumulated_parents"
+):
+    path = output_dir / f"{filename}.parq"
+    # print(accumulated_parent_data)
+    if not SKIP_SAVE and len(accumulated_parent_data) > 0:
+        print(f"Saving Accumulated Parent Data to {path}")
+        try:
+            accumulated_parent_data_tosave = accumulated_parent_data.copy()
+            accumulated_parent_data_tosave[
+                "child_window"
+            ] = accumulated_parent_data_tosave["child_window"] / pd.to_timedelta("60s")
+
+            accumulated_parent_data_tosave.to_parquet(path)
+
+            if RUN_SAVE_TEST:
+                loaded = load_accumulated_parents(output_dir, filename)
+                print("a", accumulated_parent_data)
+                print(loaded)
+                # print(f"SAVE TEST! \n{accumulated_parent_data.compare(loaded)}")
+                assert_frame_equal(accumulated_parent_data, loaded)
+                # assert accumulated_parent_data.equals(
+                #     loaded
+                # ), f"SAVE TEST FAILED! \n{accumulated_parent_data=} \n{loaded=}"
+
+        except pyarrow.ArrowNotImplementedError as e:
+            print(accumulated_parent_data.dtypes)
+            raise e
+
+
+def load_accumulated_parents(output_dir, filename="accumulated_parents"):
+    print(f"Loading previous runs from {filename}")
+    accumulated_parents = pd.read_parquet(output_dir / f"{filename}.parq")
+    accumulated_parents["child_window"] = pd.to_timedelta(
+        accumulated_parents["child_window"], unit="min"
+    )
+
+    # print(accumulated_parents)
+    return accumulated_parents
+
+
+def partition_filename_factory(window, prefix="", suffix=""):
+    def cb(keys):
+        # year, month, day = keys
+        return f"{prefix}{window}{suffix}.parq"
+
+    return cb
+
+
+def save_results(result_data, result_charts, rootWindow=ROOT_WINDOW, output_dir=None):
+    # if isinstance(window, str):
+    #     window_minutes = pd.to_timedelta(window).total_seconds() / 60
+    # else:
+    #     window_minutes = window
+
+    result_data_df = save_result_data(output_dir, result_data, rootWindow=rootWindow)
+    extra_data_df = save_extra_data(output_dir, result_charts, rootWindow=rootWindow)
+
+    accumulated_parent_data: pd.DataFrame = result_data_df[
+        ["startDate", "endDate", "window_original", "window", "minPerCycle"]
+    ]
+    accumulated_parent_data = accumulated_parent_data.rename(
+        columns={
+            "startDate": "parent_startDate",
+            "endDate": "parent_endDate",
+            "window_original": "parent_window_original",
+            "window": "parent_window",
+            "minPerCycle": "child_minPerCycle",
+        }
+    )
+
+    return accumulated_parent_data
+
+
 def save_result_data(
     output_dir,
     result_data,
@@ -465,10 +448,10 @@ def save_result_data(
         "ifft_extrapolated_wavelength"
     ] / pd.Timedelta("60s")
     df["first_extrapolated_date"] = df["first_extrapolated_date"].astype(
-        "datetime64[ms]"
+        "datetime64[us]"
     )
-    df["startDate"] = df["startDate"].astype("datetime64[ms]")
-    df["endDate"] = df["endDate"].astype("datetime64[ms]")
+    df["startDate"] = df["startDate"].astype("datetime64[us]")
+    df["endDate"] = df["endDate"].astype("datetime64[us]")
 
     # Initialize Keys
     df["year"] = df["endDate"].dt.year.astype(int)
@@ -488,13 +471,24 @@ def save_result_data(
 
     if not SKIP_SAVE:
         try:
-            for name, group in df.groupby("rootNumber"):
-                group.to_parquet(
-                    output_dir,
-                    partition_cols=["year", "month", "day"],
-                    partition_filename_cb=partition_filename_factory(name),
-                    allow_truncated_timestamps=True,
-                )
+            for (year, month, day, rootNum), group in df.groupby(
+                ["year", "month", "day", "rootNumber"]
+            ):
+                filepath = output_dir / f"year={year}" / f"month={month}" / f"day={day}"
+                filepath.mkdir(parents=True, exist_ok=True)
+                filename = filepath / f"{rootNum}.parq"
+
+                try:
+                    existing_file = pd.read_parquet(filename)
+                    group = existing_file.append(group)
+                except (FileNotFoundError, OSError):
+                    pass
+                finally:
+                    group = group[~group.index.duplicated(keep="first")]
+                    group.to_parquet(
+                        filename,
+                        allow_truncated_timestamps=True,
+                    )
         except pyarrow.ArrowNotImplementedError as e:
             print(df.dtypes)
             raise e
@@ -519,21 +513,21 @@ def save_extra_data(
             "endDate": c["endDate"],
             "window": c["window"],
             "window_original": c["window_original"],
-            "significant_dates": c[
-                "significant_extrapolation_line"
-            ].index.to_pydatetime(),
+            "significant_dates": c["significant_extrapolation_line"]
+            .index.to_numpy()
+            .astype("datetime64[us]"),
             "significant_values": c["significant_extrapolation_line"].values,
-            "insignificant_dates": c[
-                "insignificant_extrapolation_line"
-            ].index.to_pydatetime(),
+            "insignificant_dates": c["insignificant_extrapolation_line"]
+            .index.to_numpy()
+            .astype("datetime64[us]"),
             "insignificant_values": c["insignificant_extrapolation_line"].values,
         }
         for c in result_charts
     ]
     df = pd.DataFrame(data).set_index("index")
 
-    df["startDate"] = df["startDate"].astype("datetime64[ms]")
-    df["endDate"] = df["endDate"].astype("datetime64[ms]")
+    df["startDate"] = df["startDate"].astype("datetime64[us]")
+    df["endDate"] = df["endDate"].astype("datetime64[us]")
 
     # Initialize Keys
     df["year"] = df["endDate"].dt.year.astype(int)
@@ -551,30 +545,43 @@ def save_extra_data(
     df["rootNumber"] = df["rootNumber"].astype(int)
 
     # print("CHART", df)
-    # print(df.dtypes)
+    # print("XTRA", df.dtypes)
     # return df
     if not SKIP_SAVE:
         try:
-            for name, group in df.groupby("rootNumber"):
-                group.to_parquet(
-                    output_dir,
-                    partition_cols=["year", "month", "day"],
-                    partition_filename_cb=partition_filename_factory(
-                        name, suffix=".xtrp"
-                    ),
-                    allow_truncated_timestamps=True,
+            for (year, month, day, rootNum), group in df.groupby(
+                ["year", "month", "day", "rootNumber"]
+            ):
+                filename = (
+                    output_dir
+                    / f"year={year}"
+                    / f"month={month}"
+                    / f"day={day}"
+                    / f"{rootNum}.xtrp.parq"
                 )
+
+                try:
+                    existing_file = pd.read_parquet(filename)
+                    group = existing_file.append(group)
+                except (FileNotFoundError, OSError):
+                    pass
+                finally:
+                    group = group[~group.index.duplicated(keep="first")]
+                    group.to_parquet(
+                        filename,
+                        allow_truncated_timestamps=True,
+                    )
         except pyarrow.ArrowNotImplementedError as e:
-            print(df.dtypes)
+            print("ArrowNotImplementedError", df.dtypes)
             raise e
         except pyarrow.ArrowInvalid as e:
-            print(df.dtypes)
+            print("ArrowInvalid", df.dtypes)
             raise e
     return df
 
 
 if __name__ == "__main__":
-    startDate = (pd.to_datetime("today") - pd.to_timedelta("7 days") * 52).floor("D")
+    startDate = (pd.to_datetime("today") - pd.DateOffset(years=5)).floor("D")
     endDate = (
         pd.to_datetime("today").floor("D")
         - pd.to_timedelta("1 days")
